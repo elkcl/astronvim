b9e1c92ce899fdebe9c21bc0dfc3cd1dd513ced1
Денисьев Илья <s02220082@study-00.ru-central1.internal>
2023-09-19 16:50:05 +0000

    plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,8 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
